c3c9c3949aa827bfe193c177d04c9fd023e2d046
조현일 <himzei@gmail.com>
2023-04-12 16:03:04 +0900

    chart

--- src/routes/Result.jsx
+++ src/routes/Result.jsx
@@ -4,9 +4,17 @@ import {
   Grid,
   GridItem,
   HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Select,
   Text,
   VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import { useQuery } from "react-query";
@@ -18,70 +26,88 @@ import ChartResult from "../components/ChartResult";
 export default function Result() {
   const gridTemplate = "1fr 1fr 1fr 1fr 1fr";
   const { data } = useQuery(["instaAdmin"], getAdminResult);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Layout title="인스타 인증 참가자 분석">
-      <VStack w="98%" spacing={16}>
-        <Box py="16" w="full">
-          <ChartResult />
-        </Box>
-        <HStack justifyContent="space-between" w="full" py="4">
-          <HStack>
-            <Button colorScheme={"red"}>
-              <Text>차트 다운로드</Text>
-            </Button>
-            <Link to={`${BASE_URL}/insta-admin/csv-result`}>
-              <Button colorScheme={"green"} variant="outline">
-                <Text>CSV 다운로드</Text>
+    <>
+      <Layout title="인스타 인증 참가자 분석">
+        <VStack w="98%" spacing={16}>
+          <HStack justifyContent="space-between" w="full" py="4">
+            <HStack>
+              <Button colorScheme={"red"} onClick={onOpen}>
+                <Text>차트 다운로드</Text>
               </Button>
-            </Link>
+              <Link to={`${BASE_URL}/insta-admin/csv-result`}>
+                <Button colorScheme={"green"} variant="outline">
+                  <Text>CSV 다운로드</Text>
+                </Button>
+              </Link>
+            </HStack>
+            <Box w="sm">
+              <Select placeholder="Sort By" bg="white">
+                <option value="option1">좋아요 많은 순</option>
+                <option value="option2">공유횟수 많은 수</option>
+                <option value="option3">친구소환 많은 순</option>
+              </Select>
+            </Box>
           </HStack>
-          <Box w="sm">
-            <Select placeholder="Sort By" bg="white">
-              <option value="option1">좋아요 많은 순</option>
-              <option value="option2">공유횟수 많은 수</option>
-              <option value="option3">친구소환 많은 순</option>
-            </Select>
-          </Box>
-        </HStack>
-        <VStack spacing="2" w="full">
-          <Grid
-            templateColumns={gridTemplate}
-            w="full"
-            align="center"
-            color="#1E3A56"
-          >
-            <GridItem>시간</GridItem>
-            <GridItem>인스타 게시글(개)</GridItem>
-            <GridItem>좋아요(개)</GridItem>
-            <GridItem>댓글(개)</GridItem>
-            <GridItem>친구소환(명)</GridItem>
-          </Grid>
-          {data?.map((item, index) => (
+          <VStack spacing="2" w="full">
             <Grid
-              key={index}
               templateColumns={gridTemplate}
               w="full"
               align="center"
               color="#1E3A56"
-              bg="white"
-              py="4"
-              rounded="lg"
             >
-              <GridItem>{timeFormat(item?.created_at)}</GridItem>
-              <GridItem>{item?.total_insta}</GridItem>
-              <GridItem>
-                {item?.total_likes && numberFormat(item.total_likes)}
-              </GridItem>
-              <GridItem>
-                {item?.total_comments && numberFormat(item.total_comments)}
-              </GridItem>
-              <GridItem>{item?.total_friends}</GridItem>
+              <GridItem>시간</GridItem>
+              <GridItem>인스타 게시글(개)</GridItem>
+              <GridItem>좋아요(개)</GridItem>
+              <GridItem>댓글(개)</GridItem>
+              <GridItem>친구소환(명)</GridItem>
             </Grid>
-          ))}
-          <Box h="80px" />
+            {data?.map((item, index) => (
+              <Grid
+                key={index}
+                templateColumns={gridTemplate}
+                w="full"
+                align="center"
+                color="#1E3A56"
+                bg="white"
+                py="4"
+                rounded="lg"
+              >
+                <GridItem>{timeFormat(item?.created_at)}</GridItem>
+                <GridItem>{item?.total_insta}</GridItem>
+                <GridItem>
+                  {item?.total_likes && numberFormat(item.total_likes)}
+                </GridItem>
+                <GridItem>
+                  {item?.total_comments && numberFormat(item.total_comments)}
+                </GridItem>
+                <GridItem>{item?.total_friends}</GridItem>
+              </Grid>
+            ))}
+            <Box h="80px" />
+          </VStack>
         </VStack>
-      </VStack>
-    </Layout>
+      </Layout>
+      <Modal size={"7xl"} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>차트 확인하기</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box py="16" w="full">
+              <ChartResult />
+            </Box>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
